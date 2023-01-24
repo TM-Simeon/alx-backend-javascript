953a3b3ed523ac1ea18b741eddb6da7cf968ef38
TM-Simeon <mnaan.simeon@st.futminna.edu.ng>
2023-01-24 04:09:24 +0100

    update

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -6,8 +6,27 @@
 // console.log(taskBlock(true));
 // console.log(taskBlock(false));
 
-import getNeighborhoodsList from './2-arrow.js';
+// import getNeighborhoodsList from './2-arrow.js';
 
-const neighborhoodsList = new getNeighborhoodsList();
-const res = neighborhoodsList.addNeighborhood('Noe Valley');
-console.log(res);
+// const neighborhoodsList = new getNeighborhoodsList();
+// const res = neighborhoodsList.addNeighborhood('Noe Valley');
+// console.log(res);
+
+// import getSumOfHoods from './3-default-parameter.js';
+
+// console.log(getSumOfHoods(34));
+// console.log(getSumOfHoods(34, 3));
+// console.log(getSumOfHoods(34, 3, 4));
+
+// import returnHowManyArguments from './4-rest-parameter.js';
+
+// console.log(returnHowManyArguments("one"));
+// console.log(returnHowManyArguments("one", "two", 3, "4th"));
+
+// import concatArrays from './5-spread-operator.js';
+
+// console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
+
+import getSanFranciscoDescription from "./6-string-interpolation.js";
+
+console.log(getSanFranciscoDescription())
